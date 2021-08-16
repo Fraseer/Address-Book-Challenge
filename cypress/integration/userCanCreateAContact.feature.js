3a75a6db4224b1a73594f2c6b06e89c75aa80d13
Fraser <Fraser.Hughes@btinternet.com>
2021-08-16 22:30:15 +0200

    Creates input for contact

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -1,5 +1,12 @@
 describe('user can create a contact', () => {
   it('test', () => {
     cy.visit('http://localhost:3001')
-  })  
+    cy.get('#add-contact').click()
+    cy.get("#name").type("Fraser Hughes");
+    cy.get('#email').type('Fraser.Hughes@btinternet.com');
+    cy.get('#phone').type('1234 567890');
+    cy.get('#company').type('Craft Academy');
+    cy.get('#notes').type('Thats me!');
+    cy.get('#twitter').type('@Fraser_Hughes_');
+  });
 });
